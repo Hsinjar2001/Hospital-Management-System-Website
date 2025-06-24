115ff6c94cdcb75dbed46788d2ef1f8f6b3cca76
Rajnish Kumar Saphi <rajnishrajak82@gmail.com>
2025-06-24 18:56:03 +0545

    AdminDoctorsPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Registerpage from './Component/Public/Register_page'
 import Forgetpasswordpage from './Component/Public/ForgetPassword_page'
 import Errorpage from './Component/Public/Errorpage_404_page'
 import { Routes, Route } from 'react-router-dom'
+import AdminDoctorsPage from './Component/Public/AdminDoctors_page'
 
 
 function App() {
@@ -20,7 +21,8 @@ function App() {
    <Route  path='/login' element= {   <Loginpage/>}/>   
    <Route  path='/register' element= { <Registerpage/>}/>  
    <Route  path='/forgetpassword' element= { <Forgetpasswordpage/>}/>  
-<Route path='/errorpage' element={<Errorpage/>}/>
+  <Route path='/errorpage' element={<Errorpage/>}/>
+  <Route path='/AdminDoctors' element={<AdminDoctorsPage/>}/>
 
    
     </Routes>
